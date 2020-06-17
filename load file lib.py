e947cbf902affa968c17449570925d92688b5f0c
agit8ed <45208322+agit8ed@users.noreply.github.com>
2020-06-17 10:55:39 +0300

    Update load file lib.py

--- load file lib.py
+++ load file lib.py
@@ -1,3 +1,7 @@
+import pygame
+import os
+
+
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
     try:
@@ -36,3 +40,21 @@ def mirror_center(image, rect, x_tr, y_tr):
     mirr_image = pygame.transform.flip(image, x_tr, y_tr)
     mirr_rect = mirr_image.get_rect(center=rect.center)
     return mirr_image, mirr_rect
+
+
+def scales_and_mirrors(image, scale_x, scale_y, mirror_flag, mirror_x=None, mirror_y=None):
+    if mirror_flag is False:
+        return pygame.transform.scale(load_image(image), (scale_x, scale_y))
+    else:
+        return pygame.transform.scale(mirror_center(load_image(image), load_image(image).get_rect(),
+                                                    mirror_x, mirror_y)[0], (scale_x, scale_y))
+
+
+def scales_and_rots(image, scale_x, scale_y, angle):
+    return pygame.transform.scale(rot_center(load_image(image), load_image(image).get_rect(), angle)[0],
+                                  (scale_x, scale_y))
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
