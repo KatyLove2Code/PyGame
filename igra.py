74a1b9964cd7e76ee7b70ee014658f854dbc1a2e
iliakuzmich <kuzmich.il@yandex.ru>
2020-06-20 20:50:27 +0300

    вопрос, почему он ругается на y?
    возможно нужно y еще в файл с со стенами закинуть, но чет не робит, да и не логично это

--- igra.py
+++ igra.py
@@ -1,6 +1,5 @@
 """
-крч, я не знаю, где ошибки, но он либо не ходит, либо не обновляет картинку
-помогите пожалуйста
+место под описание ошибок
 """
 import pygame
 from pygame import *
@@ -10,8 +9,8 @@ pygame.init()
 
 
 # Объявляем переменные
-wight = 800  # Ширина окна (это пишется, как "width", друг мой)
-height = 640  # Высота окна
+wight = 1280  # Ширина окна
+height = 768  # Высота окна
 display = (wight, height)
 bg_color = "#000000"
 PLATFORM_WIDTH = 32
@@ -20,15 +19,17 @@ PLATFORM_COLOR = "#ffffff"
 fps=pygame.time.Clock()
 player_group = pygame.sprite.Group()  # умоляю, пользуйтесь спрайт группами!
 platform_group = pygame.sprite.Group()
+entities = pygame.sprite.Group() # Все объекты
+platforms = [] # то, во что мы будем врезаться или опираться
 level = [
        "-------------------------",
        "-                       -",
-       "-         ----          -",
+       "-                       -",
        "-                       -",
        "-            --         -",
        "-                       -",
        "--                      -",
-       "-          ----         -",
+       "-                       -",
        "-                   --- -",
        "-                       -",
        "-                       -",
@@ -37,7 +38,7 @@ level = [
        "-   -----------         -",
        "-                       -",
        "-                -      -",
-       "-          ----     --  -",
+       "-                   --  -",
        "-                       -",
        "-                       -",
        "-------------------------"]
@@ -49,24 +50,22 @@ def main():
     bg = Surface((wight, height))  # Создание видимой поверхности для фона
     bg.fill(Color(bg_color))  # Заливаем поверхность
     hero = Player(player_group, 55, 55)  # создаем героя по x,y координатам
+    entities.add(hero)
     left = right = up = False  # по умолчанию стоим
 
     # bg = pygame.Surface((wight, height))
     # bg.fill(pygame.Color(bg_color))
-    """    так, это перенесоно вниз, уровень рисует, теперь нужно вместо простого рисование создать объекты
     x = y = 0
     for row in level:
         for col in row:
             if col == "-":
-                platform = Platform(platform_group, x, y)
-                # создаем блок, заливаем его цветом и рисеум его
-                pf = pygame.Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-                pf.fill(pygame.Color(PLATFORM_COLOR))
-                screen.blit(pf, (x, y))
+                pf = Platform(x, y)
+                entities.add(pf)
+                platforms.append(pf)
             x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
         y += PLATFORM_HEIGHT  # то же самое и с высотой
         x = 0  # на каждой новой строчке начинаем с нуля
-    """
+
     while 1:  # Основной цикл программы
         for event in pygame.event.get():  # Обрабатываем события
             if event.type == QUIT:
@@ -87,17 +86,6 @@ def main():
                 left = False
 
         screen.fill(pygame.Color("black"))  # специально для обновления экрана
-        x = y = 0
-        for row in level:
-            for col in row:
-                if col == "-":
-                    # создаем блок, заливаем его цветом и рисеум его
-                    pf = pygame.Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-                    pf.fill(pygame.Color(PLATFORM_COLOR))
-                    screen.blit(pf, (x, y))
-                x += PLATFORM_WIDTH  # блоки платформы ставятся на ширине блоков
-            y += PLATFORM_HEIGHT  # то же самое и с высотой
-            x = 0  # на каждой новой строчке начинаем с нуля
         player_group.draw(screen)
         platform_group.draw(screen)
         hero.update(left, right, up)  # передвижение
