b561b4af1e6d42c82f334e899ed188da52552acb
iliakuzmich <kuzmich.il@yandex.ru>
2020-06-20 21:21:43 +0300

    так, все вернул как было

--- player.py
+++ player.py
@@ -1,6 +1,8 @@
 """
-место под описание ошибок
+крч, я не знаю, где ошибки, но он либо не ходит, либо не обновляет картинку
+помогите пожалуйста
 """
+
 from pygame import *
 
 speed = 7
@@ -24,7 +26,7 @@ class Player(sprite.Sprite):
         self.onGround = False  # стою на земле или нет
         self.collide_with = collide_with  # группы, с которыми игрок может взаимодействовать
 
-    def update(self, left, right, up, platforms):
+    def update(self, left, right, up):
         if up:
             if self.onGround:  # прыгаем только когда можем оттолкнуться от земли(потом сделаю двойной прыжок(как-хз пока что))
                 self.yvel = -JUMP_POWER
@@ -51,26 +53,7 @@ class Player(sprite.Sprite):
 
         # self.onGround = False;  # Мы не знаем, когда мы на земле
         self.rect.y += self.yvel
-        self.collide(0, self.yvel, platforms)
-
         self.rect.x += self.xvel  # переносим свои положение на xvel
-        self.collide(self.xvel, 0, platforms)
 
-    def collide(self, xvel, yvel, platforms):
-        for p in platforms:
-            if sprite.collide_rect(self, p):  # если есть пересечение платформы с игроком
-
-                if xvel > 0:  # если движется вправо
-                    self.rect.right = p.rect.left  # то не движется вправо
-
-                if xvel < 0:  # если движется влево
-                    self.rect.left = p.rect.right  # то не движется влево
-
-                if yvel > 0:  # если падает вниз
-                    self.rect.bottom = p.rect.top  # то не падает вниз
-                    self.onGround = True  # и становится на что-то твердое
-                    self.yvel = 0  # и энергия падения пропадает
-
-                if yvel < 0:  # если движется вверх
-                    self.rect.top = p.rect.bottom  # то не движется вверх
-                    self.yvel = 0  # и энергия прыжка пропадает
+    '''def draw(self, screen):  # Выводим себя на экран
+        screen.blit(self.image, (self.rect.x, self.rect.y))'''
