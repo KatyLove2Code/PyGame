aedb1ec3a41b5df3a6e88be3c227b1cba34bfaf2
Ekaterina Divina <ekaterinadivina@gmail.com>
2020-06-24 13:35:44 +0300

    Create classPlatform.py

---
+++ classPlatform.py
@@ -0,0 +1,49 @@
+"""
+место под описание ошибок
+"""
+
+from pygame import *
+
+WIN_WIDTH = 1920
+WIN_HEIGHT = 1080
+BACKGROUND_COLOR = "#FFFFFF"
+PLATFORM_WIDTH = 30
+PLATFORM_HEIGHT = 30
+PLATFORM_COLOR = "#000000"
+bg = Surface((WIN_WIDTH, WIN_HEIGHT))
+Rect = bg.get_rect()
+
+
+
+
+SPIKES = "1" #шипы
+MOBS = "2" #мобы
+TREASURE = "3" #сокровище
+LEVER = "5" #рычаг который триггерится при соприкосновении с игроком и убивающий то скопление из 4 мобов слева снизу от него
+
+class Platform(sprite.Sprite):
+    def __init__(self, groups, plat_type, x, y):
+        super().__init__(groups[0], groups[1])
+        self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
+        self.rect = self.image.get_rect().move(x, y)
+
+        if plat_type == "-":
+            color = Color("#123456")
+
+        elif plat_type == SPIKES:
+            color = Color("#654321")
+
+        elif plat_type == "2":
+            color = Color("#011032")
+
+        elif plat_type == "3":
+            color = Color("#555353")
+
+        elif plat_type == "5":
+            color = Color("#125364")
+        else:
+            color = (255, 0, 255) #Если попался случайно не тот символ, то он подкрасится
+
+        self.image.fill(color)
+
+    
