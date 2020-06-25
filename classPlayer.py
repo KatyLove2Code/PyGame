442cf9f89980af066bc3c77fdaa26389b99e2acb
agit8ed <45208322+agit8ed@users.noreply.github.com>
2020-06-25 18:28:16 +0300

    Revert "Merge branch 'develop' of https://github.com/KatyLove2Code/PyGame into develop"
    This reverts commit 34abba5f56071a5251bf80ba5dcb04a306586b3a, reversing
    changes made to 3d8fc2d171494b8702e4b285cc8be0be4d717f85.

--- classPlayer.py
+++ classPlayer.py
@@ -83,7 +83,9 @@ class Player(sprite.Sprite):
             intersect = playerRange & platformRange  # https://pythonworld.ru/tipy-dannyx-v-python/mnozhestva-set-i-frozenset.html
             if self.rect.bottom == p.rect.top and len(intersect) != 0:
                 self.onGround = True
-            elif p.rect.bottom > self.rect.bottom > p.rect.top and len(intersect) != 0:
+            if self.rect.top == p.rect.bottom and len(intersect) != 0:
+                self.yvel += GRAVITY * 10
+            if p.rect.bottom > self.rect.bottom > p.rect.top and len(intersect) != 0:
                 self.rect.bottom = p.rect.top
                 self.onGround = True
         print(self.onGround)
