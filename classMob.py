af807f82f6ea86d10ce58b1d55f8e9af604b272e
[8]{8} <64355285+Ultramigel228@users.noreply.github.com>
2020-07-05 23:44:26 +0300

    Update classMob.py

--- classMob.py
+++ classMob.py
@@ -1,6 +1,8 @@
 from pygame import *
+
 PLATFORM_WIDTH = 30
 PLATFORM_HEIGHT = 30
+
 
 class Mob(sprite.Sprite):
     def __init__(self, groups, x, y):
@@ -8,26 +10,21 @@ class Mob(sprite.Sprite):
         self.image = Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
         self.image.fill(Color("#F11032"))
 
+        self.start_x = x  # Начальная позиция Х и Y
+        self.start_y = y
+        self.rect = self.image.get_rect(x=x, y=y)  # прямоугольный объект(герой)
 
-        self.startX = x  # Начальная позиция Х и Y
-        self.startY = y
-        self.rect = self.image.get_rect(x = x, y = y) # прямоугольный объект(герой)
-
-        self.xvel = 1 # скорость бега
-        self.health = 40 #Здоровье
-        #Запоминаем старовую точку отсчёта
-        self.damage_timer = time.get_ticks() #https://www.pygame.org/docs/ref/time.html#pygame.time.get_ticks
+        self.x_vel = 1  # скорость бега
+        self.health = 40  # Здоровье
+        # Запоминаем старовую точку отсчёта
+        self.damage_timer = time.get_ticks()  # https://www.pygame.org/docs/ref/time.html#pygame.time.get_ticks
         self.maxDistance = 24
 
+    def update(self, hero):
+        self.rect.x += self.x_vel
 
-
-    def update(self, *args):
-
-        self.rect.x += self.xvel
-
-        if abs(self.startX - self.rect.x) >= self.maxDistance:
-
-
-            self.xvel *= -1
-
-
+        if abs(self.start_x - self.rect.x) >= self.maxDistance:
+            self.x_vel *= -1
+        if sprite.collide_rect(self, hero):
+            hero.health -= 10
+            self.kill()
