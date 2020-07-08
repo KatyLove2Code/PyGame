95d0074c17ed57cdbfe4568b47c5bdd64df81925
[8]{8} <64355285+Ultramigel228@users.noreply.github.com>
2020-07-08 20:46:03 +0300

    Update classWeapon.py

--- classWeapon.py
+++ classWeapon.py
@@ -6,11 +6,10 @@ WIN_HEIGHT = 1080
 PLATFORM_WIDTH = 30
 PLATFORM_HEIGHT = 30
 
+BULLET_SPEED = 15
 
 
-
-
-class Weapon(sprite.Sprite):
+'''class Weapon(sprite.Sprite):
     def __init__(self, groups, hero):
         super().__init__(groups)
         self.image = image.load('data/sword.png')
@@ -18,9 +17,30 @@ class Weapon(sprite.Sprite):
         self.hero = hero
         self.rect = self.image.get_rect()
 
-
     def update(self):
-        self.rect.bottomleft = self.hero.rect.center
+        self.rect.bottomleft = self.hero.rect.center'''
+
+
+class Bullet(sprite.Sprite):
+    def __init__(self, groups, hero):
+        super().__init__(groups[0], groups[1])
+        self.image = Surface((5, 3))
+        self.rect = self.image.get_rect()
+        self.direction = hero.direction
+        if self.direction:
+            self.rect.midleft = hero.rect.midright
+        else:
+            self.rect.midright = hero.rect.midleft
+
+    def update(self, enemy_group, platform_group):
+        self.rect.x += self.direction * BULLET_SPEED
+        for e in enemy_group:
+            if sprite.collide_rect(self, e):
+                e.kill()
+                self.kill()
+        for p in platform_group:
+            if sprite.collide_rect(self, p):
+                self.kill()
 
 
 
