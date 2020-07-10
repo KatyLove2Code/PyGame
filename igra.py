51e53efe1b1505db96b25c07019d1d25f9aea369
[8]{8} <64355285+Ultramigel228@users.noreply.github.com>
2020-07-10 13:05:21 +0300

    Update igra.py

--- igra.py
+++ igra.py
@@ -95,6 +95,10 @@ def main():
     current_bullets = 10
     disable_keyboard = False
     death_delay = 0
+<<<<<<< HEAD
+=======
+    current_bullets = 20
+>>>>>>> 670a60457bb503e1ad93bf171f205aa3309efd1f
     screen = pygame.display.set_mode(display, FULLSCREEN)
     pygame.display.set_caption("ultra_game")
     global num_of_level
@@ -130,7 +134,7 @@ def main():
             if death_delay == 30:
                 disable_keyboard = False
                 restart_level(hero, screen)
-                current_bullets = 10
+                current_bullets = 20
                 death_delay = 0
             death_delay += 1
 
