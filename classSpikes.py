9fee9aa2ab20f568ef6e012506515d60880bdf76
[8]{8} <64355285+Ultramigel228@users.noreply.github.com>
2020-07-12 23:55:46 +0300

    Update classSpikes.py

--- classSpikes.py
+++ classSpikes.py
@@ -1,77 +1,36 @@
 import pygame
 from pygame import *
-from load_file_lib import load_image, scales_and_mirrors
+from random import choice
 
-WIDTH = 800
-HEIGHT = 600
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-FPS = 60
-
-spike_group = pygame.sprite.Group()
-all_sprites = pygame.sprite.Group()
+PLATFORM_WIDTH = 30
+SPIKE_SPEED = 1
+spike_image = transform.scale(image.load("textures/spikes1.png"), (30, 30))
 
 
 class Spikes(pygame.sprite.Sprite):
-    def __init__(self, group, coords, spike_speed=0):
-        super().__init__(group)
-        self.image = transform.scale(image.load("textures/spikes.png"), (30, 30))
-        self.rect = self.image.get_rect().move(coords)
-        self.init_coord = coords  # spike position
-        self.spike_speed = spike_speed  # it would be more appropriate to name it "spike slowness"
-        self.spikes_out = True  # the state of the spikes
-        self.spike_counter = spike_speed
-        self.spike_anim_timer = 0
+    def __init__(self, group, x, y):
+        super().__init__(group[0], group[1])
+        self.start_x = x
+        self.start_y = y
+        self.spike_status = choice([-1, 1])
+        self.height = 30 if self.spike_status > 0 else 0
+        self.image = spike_image.subsurface((0, 0, PLATFORM_WIDTH, self.height))
+        self.rect = self.image.get_rect(x=x, y=y + 30 - self.height)
+        self.delay = time.get_ticks()
 
-    def retract(self):  # haha spike go down
-        self.rect.y += int(self.rect.width / 3)
-        self.spike_anim_timer += 1
-        if self.spike_anim_timer == 3 and self.rect.y != self.init_coord[1] + self.rect.width:
-            self.rect.y = self.init_coord[1] + self.rect.width
-            self.spikes_out = False
-        elif self.spike_anim_timer == 3:
-            self.spikes_out = False
+    def update(self, hero):
+        if sprite.collide_rect(self, hero):
+            hero.damage()
+        if self.spike_status < 0 and time.get_ticks() - self.delay >= 2000 or self.height != 0:
+            self.animation()
 
-    def pop_out(self):  # haha spike go up
-        self.rect.y -= int(self.rect.width / 3)
-        self.spike_anim_timer += 1
-        if self.spike_anim_timer == 3 and self.rect.y != self.init_coord[1]:
-            self.rect.y = self.init_coord[1]
-            self.spikes_out = True
-        elif self.spike_anim_timer == 3:
-            self.spikes_out = True
+    def animation(self):
+        self.height -= SPIKE_SPEED if self.spike_status > 0 else -SPIKE_SPEED
+        if self.height == 0 or self.height == 30:
+            self.spike_status *= -1
 
-    def update(self, character=None, character_group=None):  # spikes constantly retracting
-        if self.spike_speed > 0:  # set spike speed to 0 for static spikes
-            if self.spike_counter > 0:  # a countdown for spike movement
-                self.spike_counter -= 1
-            else:
-                if self.spikes_out is True:
-                    self.retract()
-                else:
-                    self.pop_out()
-                if self.spike_anim_timer == 3:
-                    self.spike_counter = self.spike_speed
-                    self.spike_anim_timer = 0
-        if pygame.sprite.spritecollideany(self, character_group):
-            if character is not None and character_group is not None:
-                character.damage()
+        self.image = spike_image.subsurface((0, 0, PLATFORM_WIDTH, self.height))
+        self.rect = self.image.get_rect(x=self.start_x, y=self.start_y + 30 - self.height)
+        self.delay = time.get_ticks()
 
 
-def run_cycle():  # a demonstration
-    spike = Spikes(("Spikes_CD.png", 50, 50, False), spike_group, (150, 150), 150)
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        screen.fill(pygame.Color(0, 0, 0))
-        spike.update()  # you can put and the next line after the platform
-        spike_group.draw(screen)  # to see what happens
-        pygame.draw.rect(screen, (255, 0, 0), (120, 200, 250, 140))  # a "platform" for spike demonstration
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
-if __name__ == "__main__":
-    run_cycle()
